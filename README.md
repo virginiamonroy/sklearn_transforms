17df3f411df944917fa533cc4d95686eb6f8d0b5
virginiamonroy <55405849+virginiamonroy@users.noreply.github.com>
2020-08-19 21:53:00 -0500

    Primer commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# sklearn_transforms
+This Python package encapsulates custom sklearn pipeline transforms for use with the Watson Machine Learning API
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(
+      name='my_custom_sklearn_transforms',
+      version='1.0',
+      description='''
+            This is a sample python package for encapsulating custom
+            tranforms from scikit-learn into Watson Machine Learning
+      ''',
+      url='https://github.com/vnderlev/sklearn_transforms/',
+      author='Vanderlei Munhoz',
+      author_email='vnderlev@protonmail.ch',
+      license='BSD',
+      packages=[
+            'my_custom_sklearn_transforms'
+      ],
+      install_requires=[
+         'lightgbm==2.0.2'
+      ],
+      zip_safe=False
+)
+
+
